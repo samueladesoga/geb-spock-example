64ec95e8b7dac069261759c3df8fa9bc7a49f8c0
Samuel Adesoga <samueladesoga@gmail.com>
2018-11-15 08:55:00 +0000

    get and post now working

--- src/test/groovy/TShirtDesignerSpec.groovy
+++ src/test/groovy/TShirtDesignerSpec.groovy
@@ -19,10 +19,11 @@ class TShirtDesignerSpec extends GebReportingSpec {
         root.setHeaders('application/json')
 
         when: "i request the product url api"
-        def jsonObj = new JsonSlurper().parseText('{"data":{"product":{"id":"n50p-na"},"locations":[{"name":"Front","colors":0,"isFullColor":false,"colorsNum":0,"colorsList":[],"objects":[{"type":"image","isUploaded":false,"id":"first-image-sam","colorsList":[],"colorsNum":0,"colors":0,"isFullColor":false}],"objectCount":1,"letterings":0,"images":1},{"name":"Back","colors":0,"isFullColor":false,"colorsNum":0,"colorsList":[],"objects":[],"objectCount":0,"letterings":0,"images":0}],"colorsList":[],"colors":0,"isFullColor":false,"colorsNum":0,"quantities":[{"size":"S","quantity":1}],"namesNumbers":[]}}')
+        def bodyString = '{"data":{"product":{"id":"n50p-na"},"locations":[{"name":"Front","colors":0,"isFullColor":false,"colorsNum":0,"colorsList":[],"objects":[{"type":"image","isUploaded":false,"id":"first-image-sam","colorsList":[],"colorsNum":0,"colors":0,"isFullColor":false}],"objectCount":1,"letterings":0,"images":1},{"name":"Back","colors":0,"isFullColor":false,"colorsNum":0,"colorsList":[],"objects":[],"objectCount":0,"letterings":0,"images":0}],"colorsList":[],"colors":0,"isFullColor":false,"colorsNum":0,"quantities":[{"size":"S","quantity":1}],"namesNumbers":[]}}'
+        def jsonObj = new JsonSlurper().parseText(bodyString)
         def resp = root.post("customizations/get_quote", jsonObj)
 
         then: "the  products are grouped by categories"
-        assert resp['productCategoriesList'].size > 0
+        assert resp['prices'].size > 0
     }
 }
